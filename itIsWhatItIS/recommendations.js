a7e5b14735f912e312b37b7c965131f1f323b3ab
Michelle <michelleadouglas9@gmail.com>
2017-11-16 14:34:08 -0600

    Adding reviews

---
+++ itIsWhatItIS/recommendations.js
@@ -0,0 +1,413 @@
+var recommendations = [
+{
+	name: "Bubba's Cooks Country",
+	address:"6617 Hillcrest Ave, Dallas, TX 75205",
+	//photo: "http://culturemap.com/thumbnail/800x700/photos/2015/06/11/Burger-at-Kellers-Drive-in-in-Dallas_135523.jpg",
+	rating: "4.6",
+	comment: "Retro-style diner serving comfort food classics either at the counter or the drive-thru window.",
+	scores: [
+		"5",
+		"4",
+		"5",
+		"5",
+		"4"]
+},
+{
+	name: "Maple & Motor",
+	address:"4810 Maple Ave, Dallas, TX 75219",
+	//photo: "http://culturemap.com/thumbnail/800x700/photos/2015/06/11/Burger-at-Kellers-Drive-in-in-Dallas_135523.jpg",
+	rating: "4.5",
+	comment: " Bustling counter-serve diner with hip, no-frills decor, serving serious burgers and onion rings.",
+	scores: [
+		"3",
+		"5",
+		"5",
+		"4",
+		"4"]
+},
+{
+	name: "Don't Tell Supper Club",
+	address:"2626 Commerce St, Dallas, TX 75201",
+	//photo: "http://culturemap.com/thumbnail/800x700/photos/2015/06/11/Burger-at-Kellers-Drive-in-in-Dallas_135523.jpg",
+	rating: "4.3",
+	comment: " Hip, upscale, two-story digs for shareable plates, wild cocktails & circus-worthy entertainment.",
+	scores: [
+		"3",
+		"5",
+		"2",
+		"1",
+		"3"]
+},
+{
+	name: "Ferris Wheelers Backyard and BBQ",
+	address:"1950 Market Center Blvd, Dallas, TX 75207",
+	//photo: "http://culturemap.com/thumbnail/800x700/photos/2015/06/11/Burger-at-Kellers-Drive-in-in-Dallas_135523.jpg",
+	rating: "4.2",
+	comment: "Smoked brisket, ribs, sandwiches, plus full bar & live music in backyard setting with Ferris wheel..",
+	scores: [
+		"3",
+		"3",
+		"5",
+		"1",
+		"5"]
+},
+{
+	name: "Zoli's NY Pizza",
+	address:"14910 Midway Rd, Addison, TX 75001",
+	//photo: "http://culturemap.com/thumbnail/800x700/photos/2015/06/11/Burger-at-Kellers-Drive-in-in-Dallas_135523.jpg",
+	rating: "4.0",
+	comment: "Pizzeria with a straightforward menu of NYC-style pies, slices, heros & salads plus lunch specials.",
+	scores: [
+		"3",
+		"3",
+		"5",
+		"1",
+		"2"]
+},
+{
+	name: "Keller's Drive-In",
+	address:"6537 Northwest Hwy, Dallas, TX 75231",
+	//photo: "http://culturemap.com/thumbnail/800x700/photos/2015/06/11/Burger-at-Kellers-Drive-in-in-Dallas_135523.jpg",
+	rating: "4.1",
+	comment: "This old school car hop stands out by using poppyseed buns and being generous with the bacon. The service is fast and the people are down to earth.",
+	scores: [
+		"1",
+		"1",
+		"1",
+		"1",
+		"2"]
+},
+{
+	name: "Twisted Root Burger Co.",
+	address:"2615 Commerce St, Dallas, TX 75226",
+	photo: "http://media.culturemap.com/crop/7a/74/800x700/Frito-Bandito-burger-at-Twisted-Root-Burger-Co.-in-Dallas_153817.jpg",
+	rating: "4.5",
+	comment: "This place offers quite the variety in a burger from veggie to bison they have it all. Our recommendation is the Big Tex Burger!",
+	scores: [
+		"5",
+		"4",
+		"4",
+		"4",
+		"4"]
+},
+{
+	name: "Digg's Taco Shop",
+	address:"6309 Hillcrest Ave, Dallas, TX 75214",
+	photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.fivestars.com%2Fbiz_pictures%2F9983b1dfe7a14f88ac6b5691bcac0b1b.jpg&imgrefurl=http%3A%2F%2Fwww.fivestars.com%2Fbusiness%2FTX%2FDallas%2FRestaurant%2FDigg-s-Tacos-Gaston%2F2900d4e419f14e0e9f9c5523a7258b90%2F&docid=CwKx4_rblVHUaM&tbnid=SLfOeueBZm4GIM%3A&vet=10ahUKEwjEotam3bnXAhXrsVQKHb-uClkQMwhdKBUwFQ..i&w=400&h=266&bih=657&biw=693&q=digg%27s%20taco&ved=0ahUKEwjEotam3bnXAhXrsVQKHb-uClkQMwhdKBUwFQ&iact=mrc&uact=8",
+	rating: "4.4",
+	comment: "Friendly staff, and great variety on the menu. We recommend the Bhan Jovi! But careful, it has a kick!",
+	scores: [
+		"2",
+		"3",
+		"3",
+		"3",
+		"2"]
+},
+{
+	name: "Maracas Cocina Mexicana",
+	address: "2914 Main St, Dallas, TX 75226",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.3",
+	comment: "Great brunch specials, cheap mimosa's, amazing food. We recommend the banana pancakes if you are in the mood for something sweet!",
+	scores: [
+		"5",
+		"5",
+		"3",
+		"5",
+		"3"]
+},
+
+{
+	name: "Amore",
+	address: "6931 Snider Plaza, Dallas, TX 75205",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.5",
+	comment: " Intimate destination for Italian classics such as lasagna & Neapolitan pizza, plus wine & beer."
+		"5",
+		"5",
+		"3",
+		"2",
+		"3"]
+},
+
+{
+	name: "Paul Martins American Grill",
+	address: "3848 Oak Lawn Ave Suite 210, Dallas, TX 75219",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.3",
+	comment: "The first Texas outpost of this modern American chain brings an entree menu of seafood, meat and poultry with a sustainable/free-range bent to the Oak Lawn neighborhood.",
+	scores: [
+		"5",
+		"4",
+		"3",
+		"4",
+		"3"]
+},
+
+{
+	name: "Trinity Hall Irish Pub & Restaurant",
+	address: "5321 E Mockingbird Ln # 250, TX 75206",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.3",
+	comment: "Soccer on TV & live music are features of this lively spot with an Irish menu & wide beer selection.",
+	scores: [
+		"5",
+		"3",
+		"3",
+		"4",
+		"3"]
+},
+
+{
+	name: "Mi Cocina",
+	address: "77 Highland Park Village, Dallas, TX 75205",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.3",
+	comment: "A “go-to” in the metroplex for “straight-up Tex-Mex”, these “modern” cantinas keep things spicy with a big menu of traditional favorites and “potent” margaritas (after a Mambo Taxi “you’ll need a taxi” to get home); though some find them “unremarkable”, most agree they’re “always a winner.”",
+	scores: [
+		"5",
+		"5",
+		"3",
+		"5",
+		"3"]
+},
+
+{
+	name: "Maggiano's Little Italy",
+	address: "205 N Park Center, Dallas, TX 75225",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.3",
+	comment: " Semi-upscale chain for classic Italian fare served family-style in a relaxed, polished space."
+		"5",
+		"5",
+		"3",
+		"5",
+		"3"]
+},
+
+{
+	name: "Seasosns 52",
+	address: "8687 N Central Expy B307, Dallas, TX 75225",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.3",
+	comment: "Rotating menu of seasonal American dishes alongside international wines in an upscale setting",
+	scores: [
+		"5",
+		"5",
+		"3",
+		"5",
+		"3"]
+},
+
+{
+	name: "Hillstone",
+	address: "8300 Preston Rd A, Dallas, TX 75225",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.6",
+	comment: "Upscale chain eatery featuring steak, seafood & pasta alongside specialty cocktails",
+	scores: [
+		"5",
+		"5",
+		"5",
+		"5",
+		"4"]
+},
+
+{
+	name: "Velvet Taco",
+	address: "3012 N Henderson Ave, Dallas, TX 75206",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.6",
+	comment: " Tacos with global flavor inspirations plus boutique sodas & beers in hip, fast-food-style joint.",
+	scores: [
+		"5",
+		"5",
+		"3",
+		"5",
+		"3"]
+},
+
+{
+	name: "The Grape Restaurant",
+	address: "2808 Greenville Ave, Dallas, TX 75206",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.4",
+	comment: "Longtime bistro with an innovative New American menu (including brunch) in cozy, romantic environs.",
+	scores: [
+		"5",
+		"5",
+		"3",
+		"4",
+		"4"]
+},
+
+{
+	name: "Abacus",
+	address: "77 Highland Park, Dallas, TX 75205",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.3",
+	comment: "Innovative global cooking is artfully presented in a luxe, sophisticated space with an open kitchen.",
+	scores: [
+		"5",
+		"5",
+		"3",
+		"5",
+		"3"]
+},
+
+{
+	name: "Mountlake Cut",
+	address: "8220 Westchester Dr b, Dallas, TX 75225",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.4",
+	comment: "Fresh fish & raw-bar specialties headline this sophisticated Pacific Northwest-inspired destination",
+	scores: [
+		"5",
+		"5",
+		"4",
+		"5",
+		"3"]
+},
+
+{
+	name: "Gemma Restaurant",
+	address: "2323 N Henderson Ave # 109, Dallas, TX 75226",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.8",
+	comment: "Relaxed outfit decked in white & blue with a coastal California feel serving New American bites.",
+	scores: [
+		"5",
+		"5",
+		"5",
+		"5",
+		"4"]
+},
+
+{
+	name: "Eddie V's Prime Seafood",
+	address: "4023 Oak Lawn Ave, Dallas, TX 75219",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.6",
+	comment: "Elegant fine-dining chain with a seafood & steak menu, plus a cocktail lounge featuring live jazz",
+	scores: [
+		"5",
+		"5",
+		"3",
+		"5",
+		"3"]
+},
+
+{
+	name: "Shinsei",
+	address: "7713 Inwood Rd, Dallas, TX 75209",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.3",
+	comment: "Stylish Pan-Asian restaurant serving sushi & fusion dishes, plus beer, wine & sake.",
+	scores: [
+		"5",
+		"5",
+		"3",
+		"4",
+		"3"]
+},
+
+{
+	name: "Nonna",
+	address: "4115 Lomo Alto Dr, Dallas, TX 75219",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.3",
+	comment: "Hit up "Nonna" when you want an epic feast of beautiful Italian food.",
+	scores: [
+		"5",
+		"5",
+		"3",
+		"3",
+		"3"]
+},
+
+{
+	name: "Mansion Restaurant",
+	address: "2821 Turtle Creek Blvd, Dallas, TX 75219",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.4",
+	comment: "New American cuisine with regional flair presented in an elegant environment with an outdoor patio.",
+	scores: [
+		"5",
+		"5",
+		"3",
+		"5",
+		"3"]
+},
+
+{
+	name: "Sal's Pizza & Restaurant",
+	address: "2525 Wycliff Ave, Dallas, TX 75219",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.0",
+	comment: "Laid-back joint dishing up NY-style pies, heros, pasta & salads in an unpretentious space.",
+	scores: [
+		"4",
+		"4",
+		"3",
+		"4",
+		"3"]
+},
+
+{
+	name: "Celebration",
+	address: "4503 W Lovers Ln, Dallas, TX 75209",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.5",
+	comment: "Homestyle pot roast, fried chicken, cobbler & more in a relaxed, family-oriented setting.",
+	scores: [
+		"5",
+		"5",
+		"3",
+		"5",
+		"4"]
+},
+
+{
+	name: "Parigi Restaurant",
+	address: "3311 Oak Lawn Ave, Dallas, TX 75219",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.5",
+	comment: "Longtime neighborhood bistro boasts creative New American cuisine in a sophisticated atmosphere..",
+	scores: [
+		"4",
+		"5",
+		"3",
+		"4",
+		"3"]
+},
+
+{
+	name: "Mot Hai Ba",
+	address: "6047 Lewis St, Dallas, TX 75206",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.3",
+	comment: "North Vietnamese fare including pho & Sunday brunch in a small, elegantly casual space with patio.",
+	scores: [
+		"5",
+		"3",
+		"3",
+		"5",
+		"3"]
+},
+{
+	name: "Ocean Prime",
+	address: "2101 Cedar Springs Rd Ste 150, Dallas, TX 75201",
+	photo: "https://s3-media1.fl.yelpcdn.com/bphoto/XEIDHCSrtDOGc-Aywc9_EQ/258s.jpg",
+	rating: "4.4",
+	comment: "Upscale chain known for its sophisticated decor, long wine list & classic steak & seafood menu.",
+	scores: [
+		"5",
+		"3",
+		"4",
+		"5",
+		"3"]
+},
+
+];
+
+module.exports = recommendations;
